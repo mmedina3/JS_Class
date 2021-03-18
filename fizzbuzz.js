8499b8f02a2709af413cc6b98af603795a954368
mmedina3 <medina.m1016@gmail.com>
2021-03-17 20:03:19 -0700

    Assignment 1

---
+++ fizzbuzz.js
@@ -0,0 +1,2 @@
+// FizzBuzz Remastered
+// Write a program that uses console.log() to print out numbers 1 to 10 and then 10 back to 1. When going from 1 to 10, for every odd number, print "Fizz" instead of the number, and when going from 10 to 1, for every even number, print "Buzz" instead of the number.
